78fa540ac3a2351fafe8b8f2ca9d52742b2ebe81
EnergoStalin <alex.vipers@mail.ru>
2022-08-14 02:01:57 +0300

    net6 remove trash & convert indentation to tabs

--- Helpers.cs
+++ Helpers.cs
@@ -7,26 +7,26 @@ using System.Windows.Forms;
 
 namespace Anison
 {
-    static class Helpers
-    {
-        public static ToolStripMenuItem UncheckOtherToolStripMenuItems(ToolStripMenuItem selectedMenuItem)
-        {
-            selectedMenuItem.Checked = true;
+	static class Helpers
+	{
+		public static ToolStripMenuItem UncheckOtherToolStripMenuItems(ToolStripMenuItem selectedMenuItem)
+		{
+			selectedMenuItem.Checked = true;
 
-            // Select the other MenuItens from the ParentMenu(OwnerItens) and unchecked this,
-            // The current Linq Expression verify if the item is a real ToolStripMenuItem
-            // and if the item is a another ToolStripMenuItem to uncheck this.
-            foreach (var ltoolStripMenuItem in (from object
-                                                    item in selectedMenuItem.Owner.Items
-                                                let ltoolStripMenuItem = item as ToolStripMenuItem
-                                                where ltoolStripMenuItem != null
-                                                where !item.Equals(selectedMenuItem)
-                                                select ltoolStripMenuItem))
-                (ltoolStripMenuItem).Checked = false;
+			// Select the other MenuItens from the ParentMenu(OwnerItens) and unchecked this,
+			// The current Linq Expression verify if the item is a real ToolStripMenuItem
+			// and if the item is a another ToolStripMenuItem to uncheck this.
+			foreach (var ltoolStripMenuItem in (from object
+													item in selectedMenuItem.Owner.Items
+												let ltoolStripMenuItem = item as ToolStripMenuItem
+												where ltoolStripMenuItem != null
+												where !item.Equals(selectedMenuItem)
+												select ltoolStripMenuItem))
+				(ltoolStripMenuItem).Checked = false;
 
-            // This line is optional, for show the mainMenu after click
-            //selectedMenuItem.Owner.Show();
-            return selectedMenuItem;
-        }
-    }
+			// This line is optional, for show the mainMenu after click
+			//selectedMenuItem.Owner.Show();
+			return selectedMenuItem;
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -7,45 +7,45 @@ using IWshRuntimeLibrary;
 
 namespace Anison
 {
-    static class Program
-    {
-        private static string _RootPath = null;
-        public static string RootPath
-        {
-            get
-            {
-                if (_RootPath == null)
-                    _RootPath = GetRoot();
-                return _RootPath;
-            }
-            private set { }
-        }
-        private static string GetRoot()
-        {
-            if (
-                Environment.GetFolderPath(Environment.SpecialFolder.System).ToLower() == Environment.CurrentDirectory.ToLower() ||
-                Environment.GetFolderPath(Environment.SpecialFolder.Startup).ToLower() == Environment.CurrentDirectory.ToLower()
-            )
-            {
-                return Path.GetDirectoryName(Startup.GetLnkTarget(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), Path.GetFileName(Process.GetCurrentProcess().ProcessName) + ".lnk")));
-            }
-            else return Environment.CurrentDirectory;
-        }
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.ApplicationExit += OnApplicationExit;
+	static class Program
+	{
+		private static string _RootPath = null;
+		public static string RootPath
+		{
+			get
+			{
+				if (_RootPath == null)
+					_RootPath = GetRoot();
+				return _RootPath;
+			}
+			private set { }
+		}
+		private static string GetRoot()
+		{
+			if (
+				Environment.GetFolderPath(Environment.SpecialFolder.System).ToLower() == Environment.CurrentDirectory.ToLower() ||
+				Environment.GetFolderPath(Environment.SpecialFolder.Startup).ToLower() == Environment.CurrentDirectory.ToLower()
+			)
+			{
+				return Path.GetDirectoryName(Startup.GetLnkTarget(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), Path.GetFileName(Process.GetCurrentProcess().ProcessName) + ".lnk")));
+			}
+			else return Environment.CurrentDirectory;
+		}
+		/// <summary>
+		///  The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.ApplicationExit += OnApplicationExit;
 
-            Application.Run(new ContextMenu());
-        }
-        static void OnApplicationExit(object sender, EventArgs args)
-        {
-            Settings.Default.Save();
-        }
-    }
+			Application.Run(new ContextMenu());
+		}
+		static void OnApplicationExit(object sender, EventArgs args)
+		{
+			Settings.Default.Save();
+		}
+	}
 }
